6da978f2a3ac79c11736ec3638c345fe055e2afe
seahlynn <e0324409@u.nus.edu>
2020-03-28 15:59:32 +0800

    Created database, updated some queries

---
+++ db.sql
@@ -0,0 +1,11 @@
+insert into Restaurants(restid, restname, minAmt) values
+(1, '4Fingers', 15),
+(2, 'RiRi Mala', 15),
+(3, 'Yoogane', 25),
+(4, 'SushiTei', 40),
+(5, 'KFC', 10),
+(6, 'Ah Bear Mookata', 20),
+(7, 'Marche', 50),
+(8, 'HaiDiLao', 80);
+
+
