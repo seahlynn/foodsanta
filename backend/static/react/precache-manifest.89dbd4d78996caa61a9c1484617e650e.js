31010fa2262f379780b2cff452244e2eb01c0dca
Darren Sim <klassiclegend@gmail.com>
2020-04-02 01:37:52 +0800

    Set up React frontend and organise backend

---
+++ backend/static/react/precache-manifest.89dbd4d78996caa61a9c1484617e650e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00b3bc204f37e309cc2c54b30a3b26e4",
+    "url": "/static/react/../../templates/index.html"
+  },
+  {
+    "revision": "703dc782d62059801ff1",
+    "url": "/static/react/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "370c9fd060cfe3a4ac85",
+    "url": "/static/react/js/2.6d20342a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/react/js/2.6d20342a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "703dc782d62059801ff1",
+    "url": "/static/react/js/main.026a2e04.chunk.js"
+  },
+  {
+    "revision": "b75a5b5e40f71d99a173",
+    "url": "/static/react/js/runtime-main.87e018e0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/react/media/logo.5d5d9eef.svg"
+  }
+]);
